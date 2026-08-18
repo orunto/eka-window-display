f2a08a6b13fb07f6324e1898ef94a5a67aa0c4ef
Eniola Orunto <eniola@orunto.dev>
2026-08-18 11:25:50 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://xdmuayuzrveuozibgnej.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbXVheXV6cnZldW96aWJnbmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwOTU3MTIsImV4cCI6MjA4ODY3MTcxMn0.ifJJ4bgVifeamTgv4-lC8BNk0TlWZsTi2pjkVisEZC4";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
